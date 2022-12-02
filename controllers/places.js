1726f4f4fd0eaec9b29e7eb17d5264d62a051b24
Gia Madonna <giamadonna1@gmail.com>
2022-12-02 11:30:22 -0800

    New Places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,30 @@
 const router = require('express').Router()
+const places = require('../modules/places.js')
+
+router.get('/', (req,res) => {
+    res.render ('places/index', {places})
+})
 
 router.post ('/', (req,res)=> {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        //default image if one not required
+        req.body.pic = "http://placekiteen.com/400/400"
+    }
+    if (!req.body.city) {
+        //default town
+        req.body.city= "Anytown"
+    }
+    if (!req.body.state) {
+        //default state
+        req.body.state = "USA"
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-router.get('/', (req,res) => {
-    let places = [{
-        name: 'KFC',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Chicken, kountry fried',
-        pic: '/images/kountryfriedfish.avif'
-    }, {
-        name: 'Sleepy Beef',
-        city: 'Pheonix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/goodbeef.avif'
-    }]
-    res.render ('places/index', {places})
-})
 module.exports = router
 
---
+++ modules/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'KFC',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Chicken, kountry fried',
+    pic: '/images/kountryfriedfish.avif'
+}, {
+    name: 'Sleepy Beef',
+    city: 'Pheonix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/goodbeef.avif'
+}]
