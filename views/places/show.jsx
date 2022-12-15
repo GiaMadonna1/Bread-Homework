ea9876ca48af82b1b728cccb1314b1dab05dae0f
Gia Madonna <giamadonna1@gmail.com>
2022-12-14 19:41:20 -0800

    Finished part 9

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,34 +4,42 @@ const Def = require('../default')
 function show (data) {
     return (
         <Def>
-          <main>
+        <main>
             <div className='row'>
-                <div className='col-sm-6'>
-                    <img src={data.place.pic} alt= {data.place.name} />
-                        <h3>
-                            Located in {data.place.city}, {data.place.state}
-                        </h3>
-                </div>
-                <div className='col-sm-6'>
-                    <h2>
-                        Description
-                    </h2>
-                    <h3>
-                        {data.place.showEstablished()}
-                    </h3>
-                        <h4>
-                            Serving {data.place.cuisines}
-                        </h4>
-            <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
-                 Edit
-             </a>
-            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-                <button type='submit' className='btn btn-danger'>
-                    Delete
-                 </button>
-            </form>
+            <div className="col-sm-6">
+              <img src={data.place.pic} alt={data.place.name} />
+              <h3>
+                Located in {data.place.city}, {data.place.state}
+              </h3>
             </div>
-          </main>
+            <div className="col-sm-6">
+              <h1>{ data.place.name }</h1>
+              <h2>
+                Rating
+              </h2>
+             
+              <br />
+              <h2>
+                Description
+              </h2>
+              <h3>
+                {data.place.showEstablished()}
+              </h3>
+              <h4>
+                Serving {data.place.cuisines}
+              </h4>
+              <br />
+              <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                Edit
+              </a>{` `}
+              <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+              </form>
+              </div>
+            </div>
+        </main>
         </Def>
     )
 }
